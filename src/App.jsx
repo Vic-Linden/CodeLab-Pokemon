7fbf683127aa2c4c0c169d54b56b8502bab41323
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-03 12:42:36 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,41 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-const [pokemon, setPokemon] = useState(null);
-const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState(null);
+  const [search, setSearch] = useState("");
 
-const fetchPokemon = async () => {
-  const response = await fetch(
-  `https://pokeapi.co/api/v2/pokemon/${search}`
-  );
+  const fetchPokemon = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
 
-  const data = await response.json();
-  setPokemon(data);
-};
+    const data = await response.json();
+    setPokemon(data);
+  };
 
-return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button onClick={fetchPokemon}>Sök</button>
+  return (
+    <div className="container">
+      <header>
+        <h1>POKEMON</h1>
+      </header>
 
-      {pokemon && (
-        <div>
-          <h2>{pokemon.name}</h2>
-          <img src={pokemon.sprites.front_default} />
-          <p>Weight: {pokemon.weight / 10} kg</p>
-          <p>Height: {pokemon.height / 10} m</p>
+      <main>
+        <div className="card">
+          <input type="text" onChange={(e) => setSearch(e.target.value)} />
+
+          <button onClick={fetchPokemon}>Sök</button>
+
+          {pokemon && (
+            <div className="result">
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.sprites.front_default} />
+              <p>Weight: {pokemon.weight / 10} kg</p>
+              <p>Height: {pokemon.height / 10} m</p>
+            </div>
+          )}
         </div>
-      )}
+      </main>
+
+      <footer></footer>
     </div>
   );
-
 }
-
-App
+App;
