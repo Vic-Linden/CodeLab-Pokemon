ab53c11d36d456e374a371ebeb5b0c8a04d3e012
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-03 11:16:29 +0100

    it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+const [pokemon, setPokemon] = useState(null);
+const [search, setSearch] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const fetchPokemon = async () => {
+  const response = await fetch(
+  `https://pokeapi.co/api/v2/pokemon/${search}`
+  );
+
+  const data = await response.json();
+  setPokemon(data);
+};
+
+return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={fetchPokemon}>Sök</button>
+
+      {pokemon && (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} />
+          <p>Weight: {pokemon.weight / 10} kg</p>
+          <p>Height: {pokemon.height / 10} m</p>
+        </div>
+      )}
+    </div>
+  );
+
 }
 
-export default App
+App
